3ed6519298e00a04221bb3ad413daefce6d612ca
Kaspars Dambis <hi@kaspars.net>
2017-06-09 11:02:52 +0300

    Simplify the config structure

--- scripts/provision.rb
+++ scripts/provision.rb
@@ -3,21 +3,11 @@ class Veryquickstart
   def Veryquickstart.configure(config, settings)
     script_dir = File.dirname(__FILE__)
 
-    config.vm.hostname = settings["hostname"] ||= "veryquickstart"
+    config.vm.hostname = settings["hostname"] ||= "veryquickstart.local"
     config.vm.network :private_network, ip: settings["ip"] ||= "192.168.74.74"
 
-    # Keep track of all hosts assigned to this box.
-    site_hosts = []
-
-    if settings.include? 'sites'
-      settings["sites"].each do |key, site|
-        # Collect hosts for the hostupdater.
-        site_hosts.concat site["hosts"]
-      end
-    end
-
     if defined? VagrantPlugins::HostsUpdater
-        config.hostsupdater.aliases = site_hosts
+      config.hostsupdater.aliases = settings["sites"].keys
     end
 
   end
